900e08b1d9094c2fdce7807393ff7ffe5e1ba243
Jason Sooter <7215306+JasonSooter@users.noreply.github.com>
2024-10-17 11:46:08 -0600

    fix(code): add real model, update `dbt_project.yml`

--- models/example_model.sql
+++ models/example_model.sql
@@ -1,15 +1,25 @@
 -- models/example_model.sql
-WITH raw_data AS (
-  SELECT
-    id,
-    event_type,
-    event_time
-  FROM {{ ref('segment_events') }}
-)
-
+WITH
+  raw_data AS (
+    SELECT
+      *
+    FROM
+      http_api.identifies_view
+    where
+      USER_ID IS NOT NULL
+      -- SELECT
+      --   id,
+      --   event_type,
+      --   event_time
+      -- FROM {{ ref('segment_events') }}
+  )
 SELECT
-  id,
-  event_type,
-  COUNT(*) as event_count
-FROM raw_data
-GROUP BY id, event_type
+  *
+  -- id,
+  -- event_type,
+  -- COUNT(*) as event_count
+FROM
+  raw_data
+  -- GROUP BY
+  --   id,
+  --   event_type
